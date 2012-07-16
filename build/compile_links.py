9676b242ca53470eb6958fadf91e769ec96d5b89
snissn <snissn>
2012-07-16 01:33:17 -0400

    simple 404 support

--- build/compile_links.py
+++ build/compile_links.py
@@ -11,17 +11,21 @@ def get_markup_filename(match):
 def does_file_exist(filename):
   return os.path.exists(filename)
 
+
+def get_link(match):
+  html_filename = get_html_filename(match)
+  html_classes = []
+  if not does_file_exist(get_markup_filename(match)):
+    html_classes.append("error404")
+    return """<a class="{html_class}" href="./404.html#{match}md">{match}</a>""".format(html_filename=html_filename, match=match, html_class=" ".join(html_classes))
+  else: 
+    return """<a class="{html_class}" href="{html_filename}">{match}</a>""".format(html_filename=html_filename, match=match, html_class=" ".join(html_classes))
+
 for line in sys.stdin:
   matches = re.split("\[\[(.*?)\]\]", line)
   for x,n in ( (matches[n], n ) for n in xrange(len(matches)) ):
     if n % 2 == 1:
       match = matches[n]
-      html_filename = get_html_filename(match)
-      html_classes = []
-      if not does_file_exist(get_markup_filename(match)):
-        html_classes.append("error404")
-
-      match = """<a class="{html_class}" href="{html_filename}">{match}</a>""".format(html_filename=html_filename, match=match, html_class=" ".join(html_classes))
-      matches[n] = match
+      matches[n] = get_link(match)
   sys.stdout.write( ''.join(matches) )
 
