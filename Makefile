883d67896435fd17fab516c3dbb9dedc04fecd91
snissn <snissn>
2012-07-16 02:17:40 -0400

    first attempt at submodule integration, pretty sure broken

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 all:
 	@./build/build.sh
+init:
+	@git submodule add https://github.com/chjj/marked.git build/marked;cd build/marked;make;
+
 
 clean:
 	@rm *.html
--- build/build.sh
+++ build/build.sh
@@ -2,7 +2,7 @@
 set -eux
 
 export repo_directory=$HOME/r-webdev
-export marked_exec=$HOME/marked/bin/marked
+export marked_exec=$HOME/r-webdev/build/marked/bin/marked
 
 find $repo_directory -name \*.md -print0|
 xargs -0 -n1 $repo_directory/build/compile_page.sh
